67f60684c5d0ab55a12e66a8cf21a0811af8326a
César <cesar.ab@icloud.com>
2022-06-29 08:28:11 -0300

    feat(tableview): create a list to show all trees
    -list the trees, mocked
    -just the name and picture are showed, a bug will be resolved

---
+++ FreeTreeApp/Presentation/SearchView/TreeCellType.swift
@@ -0,0 +1,15 @@
+//
+//  TreeCellType.swift
+//  FreeTreeApp
+//
+//  Created by Cesar Augusto Barros on 28/06/22.
+//
+
+import UIKit
+
+class TreeCellType: UITableViewCell {
+    @IBOutlet weak var treePicture: UIImageView?
+    @IBOutlet weak var treeName: UILabel?
+    @IBOutlet weak var treeComments: UILabel?
+    @IBOutlet weak var treeDistance: UILabel?
+}
--- FreeTreeApp/Presentation/ViewMap/MapViewController.swift
+++ FreeTreeApp/Presentation/ViewMap/MapViewController.swift
@@ -17,7 +17,16 @@ class MapViewController: UIViewController {
         self.view = MapView(delegate: self)
         mapViewConfig = self.view as? MapViewConfig
         followUserLocation()
+    }
 
+    override func viewDidAppear(_ animated: Bool) {
+        let searchButton = UIButton(
+            frame: CGRect(x: 0, y: UIScreen.main.bounds.midY, width: UIScreen.main.bounds.width, height: 80))
+        searchButton.backgroundColor = .white
+        searchButton.setTitle("Search", for: .normal)
+        searchButton.addTarget(self, action: #selector(callSearchViewController), for: .touchUpInside)
+        searchButton.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+        self.view.addSubview(searchButton)
     }
 
     override func viewDidLoad() {
@@ -37,6 +46,20 @@ class MapViewController: UIViewController {
                                                  longitudinalMeters: 100)
             mapViewConfig?.setRegion(region: region)
         }
+    }
+
+    @objc func callSearchViewController() {
+        let searchVC = SearchViewController()
+        // searchVC.isModalInPresentation = true
+        if let sheet = searchVC.sheetPresentationController {
+            sheet.detents = [.medium(), .large()]
+            sheet.largestUndimmedDetentIdentifier = .medium
+            sheet.prefersScrollingExpandsWhenScrolledToEdge = false
+            sheet.prefersEdgeAttachedInCompactHeight = true
+            sheet.widthFollowsPreferredContentSizeWhenEdgeAttached = true
+
+        }
+        present(searchVC, animated: true, completion: nil)
     }
 }
 
