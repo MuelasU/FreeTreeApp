286d57fdc22ce02b5d2dc89caaeee82cb07f9462
César <cesar.ab@icloud.com>
2022-07-04 11:30:44 -0300

    My bro is alive again
    The extension UIColor was causing a infinite reclusion

--- FreeTreeApp/Structure/Extensions/UIColor+Extensions.swift
+++ FreeTreeApp/Structure/Extensions/UIColor+Extensions.swift
@@ -8,7 +8,5 @@
 import UIKit
 
 extension UIColor {
-    convenience init(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat = 1) {
-        self.init(red: red/255, green: green/255, blue: blue/255, alpha: alpha)
-    }
+
 }
