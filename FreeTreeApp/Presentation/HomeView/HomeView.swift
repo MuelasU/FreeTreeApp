27662f8f9106fbeeb21bcc70664e344883762e45
César <cesar.ab@icloud.com>
2022-07-09 11:32:58 -0300

    improvements on SearchResultList
    now we use the trees on DB

--- FreeTreeApp/Presentation/HomeView/HomeView.swift
+++ FreeTreeApp/Presentation/HomeView/HomeView.swift
@@ -11,24 +11,29 @@ struct HomeView: View {
     weak var navigationController: UINavigationController?
     @ObservedObject var viewModel: ViewModel
     @State private var searchingFor: String = ""
-    @State var index: Int = 0
+    
     
     var body: some View {
         SearchBar(profilePic: viewModel.profilePic, searchingFor: self.$searchingFor)
         if searchingFor.isEmpty {
             ScrollView {
-                FavoriteList(allTrees: viewModel.allTrees)
-                RecentList(allTrees: viewModel.allTrees)
+//                FavoriteList(allTrees: viewModel.allTrees)
+//                RecentList(allTrees: viewModel.allTrees)
             }
         } else {
             SearchResultList(trees: treeNames, searchingFor: self.$searchingFor)
         }
+        Button("Pega as arvores") {
+            viewModel.getTrees(completion: { trees in
+                    print(trees)
+                  })
+        }
     }
-    
+
     var treeNames: [String] {
         var namesOnly: [String] = []
-        for name in viewModel.allTrees {
-            namesOnly.append(name[0])
+        for name in viewModel.allTrees! {
+            namesOnly.append(name.name)
         }
         return namesOnly
     }
@@ -36,26 +41,54 @@ struct HomeView: View {
 
 extension HomeView {
     class ViewModel: ObservableObject {
+        
         var textFieldPrompt: String = "Buscar árvores"
         var leadingPadding: CGFloat = 8
         var profilePic: String = "profilePic"
+        var completion = ((Float) -> Void)?.self
+        var allTrees:[Tree]? {
+            didSet {
+                print("Arvores carregadas do servidor")
+            }
+        }
+        
         var treeName: String = ""
         var treeLocal: String = ""
         var treePic: Image = .init(systemName: "leaf")
         var isFavorite: Bool = true
         
-        var allTrees: [[String]] = [
-            ["Plantinha", "Instituto de pesquisas Eldorado", "treeExample", "yes"],
-            ["Limoeiro", "Praça do limão", "treeExample", "no"],
-            ["Pé de morango", "Praça do morango", "treeExample", "yes"]
-        ]
+
+        public func getTrees(completion: ([Tree]) -> Void) {
+            let treeServices = TreeServices()
+            treeServices.read { result in
+                switch result {
+                case let .success(trees):
+                    self.allTrees = trees
+                case let .failure(error):
+                    print("Não foi possível ler as árvores do banco \(error.localizedDescription)")
+                }
+            }
+        }
+        
+        public func printTrees() {
+            allTrees?.forEach() {
+                print("*_*_*_*_*_*_*_*_*_*_*_*_*")
+                print($0)
+            }
+        }
+        
+//        var allTrees: [[String]] = [
+//            ["Plantinha", "Instituto de pesquisas Eldorado", "treeExample", "yes"],
+//            ["Limoeiro", "Praça do limão", "treeExample", "no"],
+//            ["Pé de morango", "Praça do morango", "treeExample", "yes"]
+//        ]
     }
 }
 
-#if DEBUG
-struct SearchView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeView(viewModel: .init())
-    }
-}
-#endif
+//#if DEBUG
+//struct SearchView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        HomeView(viewModel: .init())
+//    }
+//}
+//#endif
--- FreeTreeApp/Presentation/HomeView/SearchResultList.swift
+++ FreeTreeApp/Presentation/HomeView/SearchResultList.swift
@@ -5,12 +5,13 @@
 //  Created by Cesar Augusto Barros on 05/07/22.
 //
 
+import Foundation
 import SwiftUI
 
 struct SearchResultList: View {
     @Binding var searchingFor: String
     let trees: [String]
-
+    
     init (
         trees: [String],
         searchingFor: Binding<String>
@@ -35,7 +36,7 @@ struct SearchResultList: View {
         if searchingFor.isEmpty {
             return trees
         } else {
-            return trees.filter { $0.contains(searchingFor) }
+            return trees.filter { $0.lowercased().contains(searchingFor.lowercased()) }
         }
     }
 }
