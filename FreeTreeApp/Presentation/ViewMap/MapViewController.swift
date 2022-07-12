79544daa5a38b1343c88f3cb6334a09cebfeafab
Gabriel Muelas <bimuelas@gmail.com>
2022-07-12 11:41:08 -0300

    update view model to get and set favorites

--- FreeTreeApp/Presentation/ViewMap/MapViewController.swift
+++ FreeTreeApp/Presentation/ViewMap/MapViewController.swift
@@ -11,7 +11,45 @@ import CoreLocation
 import SwiftUI
 
 class TreesStorage: ObservableObject {
-    @Published var store: [Tree] = []
+    @Published var store: [Tree] = [] {
+        didSet {
+            //Update published
+            favorites = favoritesTrees
+        }
+    }
+    @Published var favorites: [Tree] = []
+    
+    struct Configuration {
+        static let favoritesKey = "favorites"
+    }
+        
+    private var user: UserDefaults {
+        UserDefaults.standard
+    }
+    
+    private var favoritesId: [String]? {
+        user.array(forKey: Configuration.favoritesKey) as? [String]
+    }
+    
+    func addToFavorites(tree: Tree) {
+        if var favorites =  favoritesId {
+            favorites.append(tree.id!)
+            //Update user defaults
+            user.set(favorites, forKey: Configuration.favoritesKey)
+        } else {
+            user.set([tree.id!], forKey: Configuration.favoritesKey)
+        }
+        //Update published
+        self.favorites = favoritesTrees
+    }
+    
+    private var favoritesTrees: [Tree] {
+        guard let favoritesId = favoritesId else {
+            return []
+        }
+
+        return store.filter { favoritesId.contains($0.id!) }
+    }
 }
 
 class MapViewController: UIViewController {
@@ -46,7 +84,11 @@ class MapViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getTrees(completion: {_ in print("Yes")})
+        getTrees { trees in
+            if let trees = trees {
+                self.treesStorage.addToFavorites(tree: trees[0])
+            }
+        }
         addChild(sheetController)
         view.addSubview(sheetController.view)
 
@@ -80,14 +122,16 @@ class MapViewController: UIViewController {
         }
     }
 
-    public func getTrees(completion: ([Tree]) -> Void) {
+    public func getTrees(completion: @escaping ([Tree]?) -> Void) {
         let treeServices = TreeServices()
         treeServices.read { result in
             switch result {
             case let .success(trees):
                 self.treesStorage.store = trees
+                completion(trees)
             case let .failure(error):
                 print("Não foi possível ler as árvores do banco \(error.localizedDescription)")
+                completion(nil)
             }
         }
     }
